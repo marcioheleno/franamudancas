5d9cfb5129b446a55dcddf8d905e88284cb89269
marcioheleno <marcio@marcioheleno.website>
2018-01-26 09:09:12 -0300

    ajuste2 no index

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -201,6 +201,8 @@
                 </div>
             </div>
 
+
+            
             {{--<li>--}}
             {{--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>--}}
             {{--<div class="author">--}}
